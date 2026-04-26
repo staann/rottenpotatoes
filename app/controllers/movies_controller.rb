e80141759ddd80e1a6c16396ad0c3880610e2b3f
staann <gustavoch2005@gmail.com>
2026-04-26 14:43:27 -0300

    commit segunda parte

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,43 @@ class MoviesController < ApplicationController
 
   # GET /movies or /movies.json
   def index
-    @movies = Movie.all
+    @all_ratings = %w[G PG PG-13 R NC-17]
+    requested_sort = params[:sort_by] || params[:sort]
+    requested_ratings = params[:ratings]&.keys
+
+    if sortable_column?(requested_sort)
+      session[:sort_by] = requested_sort
+    elsif requested_sort.present?
+      session.delete(:sort_by)
+    end
+
+    if requested_ratings
+      session[:ratings] = requested_ratings & @all_ratings
+    end
+
+    @sort_by = sortable_column?(requested_sort) ? requested_sort : session[:sort_by]
+    @selected_ratings = requested_ratings || session[:ratings] || @all_ratings
+
+    needs_sort_redirect =
+      (params[:sort].present? && params[:sort_by].blank? && sortable_column?(requested_sort)) ||
+      (params[:sort_by].blank? && params[:sort].blank? && sortable_column?(session[:sort_by]))
+
+    needs_ratings_redirect = !params.key?(:ratings) && session[:ratings].present?
+
+    if needs_sort_redirect || needs_ratings_redirect
+      redirect_params = {}
+      redirect_params[:sort_by] = @sort_by if sortable_column?(@sort_by)
+      redirect_params[:ratings] = ratings_hash(@selected_ratings) if session[:ratings].present?
+      redirect_to movies_path(redirect_params)
+      return
+    end
+
+    @ratings_hash = session[:ratings].present? || params.key?(:ratings) ? ratings_hash(@selected_ratings) : nil
+    @movies = Movie.where(rating: @selected_ratings)
+
+    if sortable_column?(@sort_by)
+      @movies = @movies.order(@sort_by)
+    end
   end
 
   # GET /movies/1 or /movies/1.json
@@ -49,5 +85,13 @@ class MoviesController < ApplicationController
     # Only allow a list of trusted parameters through.
     def movie_params
       params.require(:movie).permit(:title, :rating, :release_date)
+    end
+
+    def sortable_column?(column_name)
+      %w[title release_date].include?(column_name)
+    end
+
+    def ratings_hash(ratings)
+      Array(ratings).index_with("1")
     end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -8,6 +8,74 @@ class MoviesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get movies_url
     assert_response :success
+    assert_select "a#title_header[href*='sort_by=title']"
+    assert_select "a#release_date_header[href*='sort_by=release_date']"
+  end
+
+  test "should sort movies by title" do
+    get movies_url, params: { sort_by: "title" }
+    assert_response :success
+
+    assert_select "th.hilite a#title_header", text: "Movie Title"
+    assert_operator response.body.index("Alpha"), :<, response.body.index("Zulu")
+  end
+
+  test "should sort movies by release date" do
+    get movies_url, params: { sort_by: "release_date" }
+    assert_response :success
+
+    assert_select "th.hilite a#release_date_header", text: "Release Date"
+    assert_operator response.body.index("Zulu"), :<, response.body.index("Alpha")
+  end
+
+  test "should keep sort in session and redirect to canonical url" do
+    get movies_url, params: { sort_by: "title" }
+    assert_response :success
+
+    get movies_url
+    assert_redirected_to movies_url(sort_by: "title")
+
+    follow_redirect!
+    assert_response :success
+    assert_select "th.hilite a#title_header", text: "Movie Title"
+  end
+
+  test "should canonicalize legacy sort param" do
+    get movies_url, params: { sort: "release_date" }
+    assert_redirected_to movies_url(sort_by: "release_date")
+
+    follow_redirect!
+    assert_response :success
+    assert_select "th.hilite a#release_date_header", text: "Release Date"
+  end
+
+  test "should filter movies by selected ratings" do
+    get movies_url, params: { ratings: { "PG" => "1" } }
+    assert_response :success
+
+    assert_includes response.body, "Alpha"
+    assert_not_includes response.body, "Zulu"
+  end
+
+  test "should keep ratings in session and redirect to canonical url" do
+    get movies_url, params: { ratings: { "PG" => "1" } }
+    assert_response :success
+
+    get movies_url
+    assert_redirected_to movies_url(ratings: { "PG" => "1" })
+
+    follow_redirect!
+    assert_response :success
+    assert_includes response.body, "Alpha"
+    assert_not_includes response.body, "Zulu"
+  end
+
+  test "should combine sorting and rating filter" do
+    get movies_url, params: { sort_by: "title", ratings: { "PG" => "1", "R" => "1" } }
+    assert_response :success
+
+    assert_select "th.hilite a#title_header", text: "Movie Title"
+    assert_operator response.body.index("Alpha"), :<, response.body.index("Zulu")
   end
 
   test "should get new" do
